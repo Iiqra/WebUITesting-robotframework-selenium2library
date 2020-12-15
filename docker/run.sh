2103d2f5e5d51f18432d9adc06ddf66a0d150f2b
Iqra Ali <iiqraali@hotmail.com>
2020-12-16 00:38:55 +0100

    added url reference in run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
+# reference URL => https://github.com/ppodgorsek/docker-robot-framework
 docker run \
     -v "$PWD/reports":/opt/robotframework/reports:Z \
     -v "$PWD/suites":/opt/robotframework/tests:Z \
